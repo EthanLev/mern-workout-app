9443042a36e9a5e583c63a8a400771ab35adda17
EthanLev <ethancolemanlevesque@gmail.com>
2025-09-02 17:35:04 -0600

    feat: set up database using MongoDB Atlas and connect with the app using Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 
 // initialize express app
@@ -17,7 +18,16 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', workoutRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('Server listening on port ' + process.env.PORT);
-})
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to DB');
+
+        // Listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('Server listening on port ' + process.env.PORT);
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+    });
